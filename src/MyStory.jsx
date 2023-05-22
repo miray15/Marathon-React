e21508637edee483d8d93fd99ed9bda6e737a904
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-22 17:52:42 -0500

    add carousel to my story page and 2 photos

--- src/MyStory.jsx
+++ src/MyStory.jsx
@@ -44,6 +44,93 @@ export function MyStory() {
           </div>
         </div>
       </nav>
+
+      <div className="container-carousel">
+        <h1>My Race Story</h1>
+        <div className="carousel-size">
+          <div
+            id="race_carousel"
+            className="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#race_carousel"
+                data-bs-slide-to="0"
+                className="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#race_carousel"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#race_carousel"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src="https://miray15.github.io/NYE-race-400px.png"
+                  className="d-block w-100"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>
+                    Some representative placeholder content for the first slide.
+                  </p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://miray15.github.io/hotcoco_400px.jpg"
+                  className="d-block w-100"
+                  alt="..."
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>
+                    Some representative placeholder content for the second
+                    slide.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#race_carousel"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#race_carousel"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
