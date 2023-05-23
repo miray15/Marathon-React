bc53a499ddd8e16b907951222830199e673aadc9
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-23 10:22:01 -0500

    add content to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,23 @@
 export function Home() {
   return (
     <div id="home-page">
-      <nav id="home-nav">
-        <div>
-          <a href="/home">Home</a>
-          <a href="#">My Story</a>
-          <a href="/donate">Donation Progress</a>
+      <nav>
+        <div className="navbar_home">
+          <a href="/index.html" className="home__page_navbar">
+            Home
+          </a>
+          <a href="/mystory.html" className="home__page_navbar">
+            My Story
+          </a>
+          <a href="donate_2.html" className="home__page_navbar">
+            Donation Progress
+          </a>
         </div>
       </nav>
 
-      <h1 className="intro">Look, its 1996!</h1>
+      <h1 className="intro">Look, it's 1996!</h1>
       <p>
-        Were going back in time, to a place where{" "}
+        We're going back in time, to a place where{" "}
         <a href="https://firstmonday.org/ojs/index.php/fm/article/view/799/708">
           Al Gore invented the internet
         </a>
@@ -25,13 +31,11 @@ export function Home() {
         </a>{" "}
       </p>
 
-      <br />
-
-      <p>Welcome to Mimis first website, to support an awesome cause!</p>
+      <p>Welcome to Mimi's first website, to support an awesome cause!</p>
 
       <h1>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
-          Why I am running the 2023 Chicago Marathon
+          Why I'm running the 2023 Chicago Marathon
         </a>
       </h1>
 
@@ -47,29 +51,10 @@ export function Home() {
         As tragic as this is, it has motivated me to take control of my health.
         I am running the 2023 Chicago Marathon for my parents, for my family,
         and for myself. <br /> As long as I live, I will do everything in my
-        power to take care of my own heart and everyones heart around me ❤️{" "}
+        power to take care of my own heart and everyone's heart around me ❤️{" "}
       </p>
 
-      <div>
-        <img
-          src="https://miray15.github.io/mom-and-dad_3.jpeg"
-          alt="Image"
-          className
-        />
-      </div>
-
-      <button onClick="">Like</button>
-      <span id="">389</span>
-
-      <br />
-      <br />
-      <p>
-        {" "}
-        Head back up to to the navigation bar to learn more about my story.
-        Click the donate button to be rediected to my fundraising site.{" "}
-      </p>
+      <img src="https://miray15.github.io/mom-and-dad_3.jpeg" alt="Image" />
     </div>
   );
 }
-
-// render.com -- host front, back, and db
