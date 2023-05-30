25e4825084e17988313535b7b8116fd4347bbdcd
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-30 11:25:08 -0500

    replace vertical with horizontal fundraising thermometer

--- src/Donate.jsx
+++ src/Donate.jsx
@@ -59,19 +59,22 @@ export function Donate() {
         </p>
       </div>
       <div id="countdown-wrap">
-        <div id="goal">$1,250</div>
+        <div id="goal">$10,000</div>
         <div id="glass">
-          <div id="progress">$35</div>
+          <div id="progress"></div>
         </div>
         <div className="goal-stat">
-          <span className="goal-number">100%</span>
+          <span className="goal-number">16%</span>
           <span className="goal-label">Funded</span>
         </div>
         <div className="goal-stat">
-          <span className="goal-number">$1,250</span>
+          <span className="goal-number">$1,640</span>
           <span className="goal-label">Raised</span>
         </div>
         <div className="goal-stat">
+          <span className="goal-number">
+            <div id="countdown"></div>
+          </span>
           <span className="goal-label">Days to Go</span>
         </div>
         <div className="goal-stat">
