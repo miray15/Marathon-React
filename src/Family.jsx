3ecc1c453b0723d92fef114847b9bb88f472eb71
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-30 17:04:15 -0500

    add background image to family page

--- src/Family.jsx
+++ src/Family.jsx
@@ -2,8 +2,44 @@ import React from "react";
 
 export function Family() {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `url("https://i.postimg.cc/sxxtV2dr/SCAN0088-2.jpg")`,
+      }}
+    >
       <p>Does this work?</p>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
