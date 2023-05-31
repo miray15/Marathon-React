074d5f131b91a4b8c5fbd2b7ca7462def89a65ec
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-30 20:24:56 -0500

    add dad content to family page

--- src/Family.jsx
+++ src/Family.jsx
@@ -12,37 +12,32 @@ export function Family() {
         <br />
         <br />
         <br />
-        <h3 className="family-text">Mama</h3>
-        <p className="family-text">
-          When my mom died my sophomore year of college, I went into shock for
-          six months. An entire semester had passed where I floated through the
-          world, disconnected from my feelings. In January, I crashed. I skipped
-          2 weeks of classes and hid. I would never receive a text that ended in
-          "luv u mama". I would never hear her voice, taste her food, or rest my
-          head on her shoulder. I nearly dropped out of college, but I wanted to
-          graduate to fulfill my promise to her.
+        <br />
+        <h3 className="mama-text">She was 58</h3>
+        <p className="mama-text">
+          When my mom died my sophomore year of college, I was in shock.{" "}
         </p>
+        <p className="mama-text">
+          I would never receive a text that ended in "luv u mama" again. I would{" "}
+        </p>{" "}
+        <p className="mama-text">
+          never hear her voice, taste her food, or rest my head on her shoulder.{" "}
+        </p>{" "}
+        <p className="mama-text">
+          I nearly dropped out of college, but I wanted to graduate to fulfill{" "}
+        </p>{" "}
+        <p className="mama-text">my promise to her.</p>
+        <br />
+        <br />
+        <h3 className="baba-text">He was 66</h3>
+        <p className="baba-text">Baba the man that was supposed to walk me </p>
+        <p className="baba-text">
+          down the aisle. He loved me unconditionally,{" "}
+        </p>
+        <p className="baba-text">and while we didn't always see eye to eye, </p>{" "}
+        <p className="baba-text">I always knew he had the best intentions</p>
       </div>
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
       <br />
       <br />
       <br />
