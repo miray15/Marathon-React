5c9d6132dc8095f050b2e897419fc4c0270ce6f2
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-30 19:53:03 -0500

    add text to mama and baba page

--- src/Family.jsx
+++ src/Family.jsx
@@ -7,13 +7,23 @@ export function Family() {
         backgroundImage: `url("https://i.postimg.cc/sxxtV2dr/SCAN0088-2.jpg")`,
       }}
     >
-      <p>Does this work?</p>
+      <div className="container">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h3 className="family-text">Mama</h3>
+        <p className="family-text">
+          When my mom died my sophomore year of college, I went into shock for
+          six months. An entire semester had passed where I floated through the
+          world, disconnected from my feelings. In January, I crashed. I skipped
+          2 weeks of classes and hid. I would never receive a text that ended in
+          "luv u mama". I would never hear her voice, taste her food, or rest my
+          head on her shoulder. I nearly dropped out of college, but I wanted to
+          graduate to fulfill my promise to her.
+        </p>
+      </div>
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
       <br />
       <br />
       <br />
