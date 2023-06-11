80747ff047de9e6e69bd6e9eeb8ec89f5d767b8b
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-11 14:32:02 -0500

    make family page background responsive

--- src/Family.jsx
+++ src/Family.jsx
@@ -8,46 +8,27 @@ export function Family() {
   };
   return (
     <div
-      className="container"
+      className="parents_background"
       style={{
         backgroundImage: `url("https://i.postimg.cc/sxxtV2dr/SCAN0088-2.jpg")`,
       }}
     >
       <div className="container">
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
         <h3 className="mama-text">She was 58</h3>
         <p className="mama-text">
-          When my mom died my sophomore year of college, I was in shock.{" "}
+          When my mom died my sophomore year of college, I would never hear her
+          voice, taste her food, or rest my head on her shoulder. It was the
+          most devastating thing that happened to me.
         </p>
-        <p className="mama-text">
-          I would never receive a text that ended in "luv u mama" again. I would{" "}
-        </p>{" "}
-        <p className="mama-text">
-          never hear her voice, taste her food, or rest my head on her shoulder.{" "}
-        </p>{" "}
-        <p className="mama-text">
-          I nearly dropped out of college, but I wanted to graduate to fulfill{" "}
-        </p>{" "}
-        <p className="mama-text">my promise to her.</p>
         <br />
+        <br /> <br />
         <br />
         <h3 className="baba-text">He was 66</h3>
         <p className="baba-text">
           My dad wanted the best for me, and worked hard to design a future
-          where{" "}
-        </p>{" "}
-        <p className="baba-text">
-          I would feel success. He learned to bake the best bread, make wheels
-          of cheese{" "}
-        </p>{" "}
-        <p className="baba-text">
-          {" "}
-          he shared with family and friends, and loved me and my brothers
-          endlessly.
+          where I would feel success. He learned to bake the best bread, make
+          wheels of cheese he shared with family and friends, and loved me and
+          my brothers endlessly.
         </p>
       </div>
       <div>
--- src/MyStory.jsx
+++ src/MyStory.jsx
@@ -7,6 +7,7 @@ export function MyStory(props) {
   const handleButtonClick = () => {
     setLikeCount(likeCount + 1);
   };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -34,7 +35,6 @@ export function MyStory(props) {
                   Home
                 </a>
               </li>
-
               <li className="nav-item">
                 <a className="nav-link" href="donate_2.html">
                   Donation Progress
@@ -53,157 +53,33 @@ export function MyStory(props) {
         </div>
       </nav>
       <h1>My Race Story</h1>
-      <div className="container-carousel">
-        <div className="carousel-size">
-          <div
-            id="race_carousel"
-            className="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-indicators">
-              <button
-                type="button"
-                data-bs-target="#race_carousel"
-                data-bs-slide-to="0"
-                className="active"
-                aria-current="true"
-                aria-label="Slide 1"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#race_carousel"
-                data-bs-slide-to="1"
-                aria-label="Slide 2"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#race_carousel"
-                data-bs-slide-to="2"
-                aria-label="Slide 3"
-              ></button>
-            </div>
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img
-                  src="https://miray15.github.io/NYE-race-400px.png"
-                  className="race-image"
-                  alt="..."
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>
-                    Some representative placeholder content for the first slide.
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img
-                  src="https://miray15.github.io/IMG_3938.JPG"
-                  className="race-image"
-                  alt="..."
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>
-                    Some representative placeholder content for the first slide.
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="https://miray15.github.io/hotcoco_400px.jpg"
-                  className="race-image"
-                  alt="..."
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>
-                    Some representative placeholder content for the second
-                    slide.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#race_carousel"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#race_carousel"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-        </div>
-        <p className="mystory">
-          When I returned from China in 2017, I realized I needed a sport to
-          maintain my fitness and healthy habits. Running is easily one of the
-          most accessibles sports on the planet! Grab a pair of shoes and run!
-          As I got more competitive, I invested in a pair of shoes that matched
-          the needs of my feet, and since then have been updating them every 300
-          miles. I also began continued biking for my{" "}
-        </p>
-      </div>
-
-      {/* <div>
-        <button onClick={handleButtonClick}>Like</button>
-        <p>{likeCount} Likes</p>
-      </div> */}
 
       <h2 className="donate-subtitle">Lifestyle changes</h2>
-      <p className="mystory">
-        When I returned from China in 2017, I realized I needed a sport to
-        maintain my fitness and healthy habits. Running is easily one of the
-        most accessibles sports on the planet! Grab a pair of shoes and run! As
-        I got more competitive, I invested in a pair of shoes that matched the
-        needs of my feet, and since then have been updating them every 300
-        miles. I also began continued biking for my{" "}
-      </p>
-      <p>
-        My mother died of complications from diabetes, so in the last few years
-        I&apos;ve been paying close attention to my A1C. Realizing that making
-        lifestyle changes later in life would be more challenging than making
-        them now, I started following the Mediterranean Diet: eliminating all
-        meat except for seafood, and maintaining a strong intake of nuts and
-        seeds
-      </p>
-      <p>
-        This proved to be successful in lowering my A1C! I am in the lowest
-        bracket of the average range!
-      </p>
-
-      {/* online subscribe button js */}
-
-      <script
-        async
-        data-uid="2677c98a82"
-        src="https://runmimirun.ck.page/2677c98a82/index.js"
-      ></script>
-
-      {/* html  */}
-      <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+      <div className="nye_race">
+        <img src="https://miray15.github.io/removed_backgroun2.jpg" alt="" />
+      </div>
 
       {props.pictureComments.map((picture) => (
         <div key={picture.id}>
           <h2>{picture.body}</h2>
         </div>
       ))}
+
+      <p>
+        When I returned from China in 2017, I realized I needed a sport to
+        maintain my fitness and healthy habits. Running is easily one of the
+        most accessible sports on the planet! Grab a pair of shoes and run! As I
+        got more competitive, I invested in a pair of shoes that matched the
+        needs of my feet, and since then have been updating them every 300
+        miles. I also began continued biking for My mother died of complications
+        from diabetes, so in the last few years I've been paying close attention
+        to my A1C. Realizing that making lifestyle changes later in life would
+        be more challenging than making them now, I started following the
+        Mediterranean Diet: eliminating all meat except for seafood, and
+        maintaining a strong intake of nuts and seeds This proved to be
+        successful in lowering my A1C! I am in the lowest bracket of the average
+        range!
+      </p>
 
       <h1>New comment</h1>
       <form>
