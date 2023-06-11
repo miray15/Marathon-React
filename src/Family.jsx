05a959e216f83dc3c6bbd8ff9619629c13cee20a
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-11 18:35:39 -0500

    add navbar to family page

--- src/Family.jsx
+++ src/Family.jsx
@@ -7,29 +7,48 @@ export function Family() {
     setLikeCount(likeCount + 1);
   };
   return (
-    <div className="parents_background">
-      <br />
-      <h3 className="mama-text">She was 58</h3>
-      <p className="mama-text">
-        When my mom died my sophomore year of college, I would never hear her
-        voice, taste her food, or rest my head on her shoulder. It was the most
-        devastating thing that happened to me.
-      </p>
-      <br />
-      <br /> <br />
-      <br />
-      <br /> <br />
-      <br />
-      <h3 className="baba-text">He was 66</h3>
-      <p className="baba-text">
-        My dad wanted the best for me, and worked hard to design a future where
-        I would feel success. He learned to bake the best bread, make wheels of
-        cheese he shared with family and friends, and loved me and my brothers
-        endlessly.
-      </p>
-      <div>
-        <button onClick={handleLike}>Like</button>
-        <p>{likeCount} Likes</p>
+    <div>
+      <nav className="navbar">
+        <ul className="navbar-menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="parents_background">
+        <br />
+        <h3 className="mama-text">She was 58</h3>
+        <p className="mama-text">
+          When my mom died my sophomore year of college, I would never hear her
+          voice, taste her food, or rest my head on her shoulder. It was the
+          most devastating thing that happened to me.
+        </p>
+        <br />
+        <br /> <br />
+        <br />
+        <br /> <br />
+        <br />
+        <h3 className="baba-text">He was 66</h3>
+        <p className="baba-text">
+          My dad wanted the best for me, and worked hard to design a future
+          where I would feel success. He learned to bake the best bread, make
+          wheels of cheese he shared with family and friends, and loved me and
+          my brothers endlessly.
+        </p>
+        <div>
+          <button onClick={handleLike}>Like</button>
+          <p>{likeCount} Likes</p>
+        </div>
       </div>
     </div>
   );
