f2bbb4e911b9bfcbc9d11d347875adcd330be2e3
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-11 17:53:59 -0500

    make my story page dynamic to different resolutions using columsn css

--- src/MyStory.jsx
+++ src/MyStory.jsx
@@ -59,33 +59,7 @@ export function MyStory(props) {
         </div>
       </nav>
       <h1>My Race Story</h1>
-      {/* <div className="image-container">
-        <img
-          src="https://i.ibb.co/2FpbZkN/WWUR2439.jpg"
-          style={{ marginRight: "10px", height: "100%", objectFit: "cover" }}
-        />
-        <div className="text-container">
-          <p>
-            When I returned from China in 2017, I realized I needed a sport to
-            maintain my fitness and healthy habits. Running is easily one of the
-            most accessible sports on the planet! Grab a pair of shoes and run!
-            As I got more competitive, I invested in a pair of shoes that
-            matched the needs of my feet, and since then have been updating them
-            every 300 miles. I also began continued biking for My mother died of
-            complications from diabetes, so in the last few years I've been
-            paying close attention to my A1C. Realizing that making lifestyle
-            changes later in life would be more challenging than making them
-            now, I started following the Mediterranean Diet: eliminating all
-            meat except for seafood, and maintaining a strong intake of nuts and
-            seeds This proved to be successful in lowering my A1C! I am in the
-            lowest bracket of the average range!
-          </p>
-        </div>
-        <img
-          src="https://miray15.github.io/removed_backgroun2.jpg"
-          className="image-container-pic2"
-        />
-      </div> */}
+
       <div className="image-container">
         <img
           src="https://i.ibb.co/2FpbZkN/WWUR2439.jpg"
@@ -115,7 +89,7 @@ export function MyStory(props) {
         </div>
         <img
           src="https://miray15.github.io/removed_backgroun2.jpg"
-          className="image-container-pic2"
+          className="image-container"
           style={{ height: "100%", width: "50%", objectFit: "cover" }}
         />
       </div>
