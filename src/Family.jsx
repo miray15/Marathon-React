6487c9f2da284c9028265399707d9d225768eb48
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-11 16:00:28 -0500

    make family photo responsive but with wacky margins

--- src/Family.jsx
+++ src/Family.jsx
@@ -7,30 +7,23 @@ export function Family() {
     setLikeCount(likeCount + 1);
   };
   return (
-    <div
-      className="parents_background"
-      style={{
-        backgroundImage: `url("https://i.postimg.cc/sxxtV2dr/SCAN0088-2.jpg")`,
-      }}
-    >
-      <div className="container">
-        <h3 className="mama-text">She was 58</h3>
-        <p className="mama-text">
-          When my mom died my sophomore year of college, I would never hear her
-          voice, taste her food, or rest my head on her shoulder. It was the
-          most devastating thing that happened to me.
-        </p>
-        <br />
-        <br /> <br />
-        <br />
-        <h3 className="baba-text">He was 66</h3>
-        <p className="baba-text">
-          My dad wanted the best for me, and worked hard to design a future
-          where I would feel success. He learned to bake the best bread, make
-          wheels of cheese he shared with family and friends, and loved me and
-          my brothers endlessly.
-        </p>
-      </div>
+    <div className="parents_background">
+      <h3 className="mama-text">She was 58</h3>
+      <p className="mama-text">
+        When my mom died my sophomore year of college, I would never hear her
+        voice, taste her food, or rest my head on her shoulder. It was the most
+        devastating thing that happened to me.
+      </p>
+      <br />
+      <br /> <br />
+      <br />
+      <h3 className="baba-text">He was 66</h3>
+      <p className="baba-text">
+        My dad wanted the best for me, and worked hard to design a future where
+        I would feel success. He learned to bake the best bread, make wheels of
+        cheese he shared with family and friends, and loved me and my brothers
+        endlessly.
+      </p>
       <div>
         <button onClick={handleLike}>Like</button>
         <p>{likeCount} Likes</p>
