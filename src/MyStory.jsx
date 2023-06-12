0376558f2f67b0942eb82f6e7505bbf5bd89b41e
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-12 15:49:17 -0500

    fix home link on my story navbar

--- src/MyStory.jsx
+++ src/MyStory.jsx
@@ -27,7 +27,7 @@ export function MyStory(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/abc">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
