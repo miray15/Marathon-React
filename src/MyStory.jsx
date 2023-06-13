9a3078db607527a7e0101ea3d611adffb54269ad
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-13 11:48:04 -0500

    adjust text on my story page

--- src/MyStory.jsx
+++ src/MyStory.jsx
@@ -12,18 +12,18 @@ export function MyStory(props) {
         <div className="text-container">
           <p>
             When I returned from Shanghai in 2017, I made it a point to continue
-            the healthy habits I started abroad. Running made the most sense!
-            All you need is a pair of shoes, and you can start your journey
-            (metaphorically and otherwise). What motivated me to make this
-            fitness change was acknowleding all the women in my life who
-            suffered from diabetes, which was one of the comorbid causes of my
-            mom&apos;s death. At 30, I began paying attention to my A1C levels,
-            adjusting my eating habits to reflect the Medditerannean diet, or
-            minimizing red meat and eating more legumes and olive oil. My A1C
-            went down since my diet change, and I&apos;m now in the lowest
-            healthy bracket for adults! If you&apos;re like me and looking to
-            make a lifestyle change, grab a pair of shoes and hit the road! Your
-            healthier self will thank you. below!
+            the healthy habits I started abroad. Running made the most sense
+            because of how accessible it is. All you need is a pair of shoes,
+            and you can start your journey (metaphorically and otherwise). What
+            motivated me to make this fitness change was acknowleding all the
+            women in my life who suffered from diabetes, which was one of the
+            comorbid causes of my mom&apos;s death. At 30, I began paying
+            attention to my A1C levels, adjusting my eating habits to reflect
+            the Medditerannean diet, or minimizing red meat and eating more
+            legumes. My A1C went down since my diet change last year, and
+            I&apos;m now in the lowest healthy bracket for adults! If
+            you&apos;re like me and looking to make a lifestyle change, grab a
+            pair of shoes and hit the road! Your healthier self will thank you!
           </p>
         </div>
         <img
