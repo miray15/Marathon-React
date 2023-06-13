2aa7d616274b3d2efff7abc65a0609ea82ea8c7a
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-13 09:00:32 -0500

    remove nav bar from my story page

--- src/MyStory.jsx
+++ src/MyStory.jsx
@@ -1,67 +1,8 @@
 import React, { useState } from "react";
 
 export function MyStory(props) {
-  // console.log(props.pictureComments);
-  // const [likeCount, setLikeCount] = useState(0);
-
-  // const handleButtonClick = () => {
-  //   setLikeCount(likeCount + 1);
-  // };
-
   return (
     <div className="mystory-div">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"></a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="/family"
-                >
-                  Family
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="/mystory"
-                >
-                  My Story
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="/donate"
-                >
-                  Donate
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <h1>My Race Story</h1>
       <div>
         <img
